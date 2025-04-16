11fc52cba0f22b9a4ba476f49511ad062079dfb5
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2025-04-16 01:22:58 +0100

    build: Bump astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-import starlightBlog from "starlight-blog";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      plugins: [starlightBlog()],
       title: "My Docs",
-      social: {
-        github: "https://github.com/withastro/starlight",
-      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
       sidebar: [
         {
           label: "Guides",
